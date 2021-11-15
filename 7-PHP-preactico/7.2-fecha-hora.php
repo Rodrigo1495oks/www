347931ee0ebaabb0bab1fb41b387f53632159808
Rodrigo1495oks <rodrir1495@gmail.com>
2021-11-15 16:54:06 -0300

    changes
    chapter 7 - updated

--- 7-PHP-preactico/7.2-fecha-hora.php
+++ 7-PHP-preactico/7.2-fecha-hora.php
@@ -1,3 +1,29 @@
 <?php
 
 
+echo time();
+
+echo time() * 7 * 24 * 60 * 60;
+
+echo mktime(0, 0, 0, 1, 1, 2000);
+
+// date($format, $timestamp);
+
+echo date("1 F jS, Y - g:ia", time());
+
+// constantes de fecha
+
+
+// ejemplo 7-3 verificacion de la validez de la fecha
+
+$month = 9;
+$day = 31;
+$year = 2022;
+
+if (checkdate($month, $day, $year)) {
+    # code...
+    echo "Date  is valid";
+} else {
+    echo "Date is invalid";
+}
+
---
+++ 7-PHP-preactico/7.3-manejo-de-archivos.php
@@ -0,0 +1,102 @@
+<?php
+
+
+
+
+// verificacion de la existencia de un archivo
+
+
+// ejemplo 7-4 creacion de un archivo sencillo de texto
+
+
+$fh = fopen("testfile.txt", "w") or die('Failed tocreate file');
+
+
+
+
+$text = "test
+ff
+ff
+f
+";
+
+fwrite($fh, $text) or die("Could´nt write to file");
+fclose($fh);
+echo "File 'testfile.txt' written succesfully";
+
+// ejemplo 7-5 - lectura de un archivo con fgets
+
+$fh = fopen("testfile.txt", "r") or die("File doesnt exist or you lack permission to open it");
+
+$line = fgets(($fh));
+
+fclose($fh);
+
+echo $line;
+
+
+// ejemplo 7-6 - klectura de un archivo con fread()
+
+$fh = fopen("testfile.txt", "r") or die("File doesnt exist or you lack permission to open it");
+$text = fread($fh, 3);
+fclose($fh);
+
+echo $text;
+
+// copia de archivos
+
+copy('testfile.txt', 'testfile2.txt');
+
+echo "File duccesfully copied to 'testfile2.txt'";
+
+// ejemplo 7-8 sintaxis alternativa para copiar un archivo
+
+if (!rename('testfile2.txt', 'testfile2.new')) {
+    echo 'could not rename file';
+} else echo "File succesfully renamed to 'testfile2.new'";
+
+
+# Eliminacion de archivos
+
+
+// ejemplo 7-10 eliminacion de un archivo
+
+
+if (unlink(!'testfile2.new')) {
+
+    echo "could not delete file";
+} else {
+    echo "file 'testfile2.new' deleted succesfully";
+}
+
+// actualizacion de archivos
+
+$fh = fopen("testfile.txt", 'r+') or die("Failed to open file");
+
+$text = fgets($fh);
+
+fseek($fh, 0, SEEK_END);
+
+fwrite($fh,"$text") or die("could not write to file");
+
+fclose($fh);
+
+echo "File 'testfile.txt' succesfully";
+
+// bloqueo de archivos
+
+// ejemplo 7-12
+
+$fh=fopen('testfile.txt','r+') or die('Failed tod open file');
+
+$text=fgets($fh);
+
+if (flock($fh,LOCK_EX)) {
+    # code...
+    fseek($fh, 0, SEEK_END);
+    fwrite($fh, "$text") or die('Could not write to file');
+    flock($fh, LOCK_UN);
+}
+fclose($fh);
+echo "File 'testfile.txt' succesfully updated";
+
