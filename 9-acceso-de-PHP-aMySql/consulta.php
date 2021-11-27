ed62f18850c8dfcad43769b48c0848cc97f297a4
Rodrigo1495oks <rodrir1495@gmail.com>
2021-11-27 16:53:47 -0300

    old

---
+++ 9-acceso-de-PHP-aMySql/consulta.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$query='SELECT * FROM classics';
+
+$result=$conn->query($query);
+
+if (!$result) {
+    die("Fatal Error");
+}
---
+++ 9-acceso-de-PHP-aMySql/query-mysqli.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'login.php';
+
+$connection=new mysqli($hn,$un,$pw,$db);
+
+$query="SELECT * FROM classics";
+$result=$connection->query($query);
+
+
+if ($connection->connect_error) {
+    die('Fatal Error');
+}
+
+$rows=$result->num_rows;
+
+for ($j=0; $j < $rows; $j++) { 
+    $result->data_seek($j);
+    
+    
+    echo 'Author: ' .htmlspecialchars($result->fetch_assoc()['author']).'<br>';
+    $result->data_seek($j);
+
+    echo 'title: ' .htmlspecialchars($result->fetch_assoc()['title']).'<br>';
+    $result->data_seek($j);
+    
+    echo 'category: ' .htmlspecialchars($result->fetch_assoc()['category']).'<br>';
+    $result->data_seek($j);
+
+    echo 'year: ' .htmlspecialchars($result->fetch_assoc()['year']).'<br>';
+    $result->data_seek($j);
+
+    echo 'Isbn: ' .htmlspecialchars($result->fetch_assoc()['isbn']).'<br>';
+    $result->data_seek($j);
+
+    echo '<br>';
+}
+
+$result->close();
+$connection->close();
+
+?>
+
