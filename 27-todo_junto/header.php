b6668b6e17c7f78a0f99580f71b86786720adcd1
Rodrigo1495oks <37790067+Rodrigo1495oks@users.noreply.github.com>
2022-02-20 02:10:26 -0300

    20/02/2022

--- 27-todo_junto/header.php
+++ 27-todo_junto/header.php
@@ -1,7 +1,52 @@
 <?php
 
+session_start();
 
+echo <<<_INIT
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="jqueryMobile/jquery.mobile-1.4.5.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+    <script src="jquery.js"></script>
 
+_INIT;
 
+require_once 'functions.php';
+
+$userstr='Welcome Guest';
+
+if (isset($_SESSION['user'])) {
+    # code...
+    $user=$_SESSION['user'];
+    $loggedin=TRUE;
+    $userstr="Logged in as: $user";
+}else{
+    $loggedin=FALSE;
+}
+
+echo <<<_MAIN
+
+_MAIN;
 
 ?>
+
+<title>Rodrigo app</title>
+</head>
+
+<body>
+    <div data-role="page">
+        <div data-role="header">
+            <div id="logo" class="center">
+                R
+                <img src="rodri.gif" alt="a" id="rodrigo">drigo Nest
+            </div>
+        </div>
+    </div>
+</body>
+
