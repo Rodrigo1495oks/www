c6f1257bd5f470fb81431fa0be5eab9824cdd5f1
Rodrigo1495oks <37790067+Rodrigo1495oks@users.noreply.github.com>
2022-03-25 19:26:13 -0300

    changes

---
+++ proyecto-asamblea/functions.php
@@ -0,0 +1,86 @@
+<?php
+
+use LDAP\Result;
+
+$hn='localhost';
+$db='empresa';
+$un='rodrigo';
+$pw='12345';
+
+$connection= new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+if ($connection->connect_error) {
+    mysqlFatalError();
+
+}
+
+function createTable($name, $query)
+{
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' ha sido creada o ya existe. <br>";
+
+}
+
+function queryMysql($query)
+{
+    global $connection;
+    $result=$connection->query($query);
+    if (!$result) {
+        # code...
+        mysqlFatalError("Consulta no realizada");
+
+    }
+
+    return $result;
+}
+function destroySession()
+{
+    $_SESSION=array();
+
+    if (session_id()!='' || isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-2592000, '/');
+    }
+}
+
+function sanitizeString($var)
+{
+    global $connection;
+    $var=strip_tags($var);
+    $var=htmlentities($var);
+
+    if (get_magic_quotes_gpc()) {
+        $var= stripslashes($var);
+
+    }
+    return $connection->real_escape_string($var);
+}
+
+function showProfile($user)
+{
+    if (file_exists("$user.jpg")) {
+        echo "<img src='$user.jpg' style='float:left'>";
+    }
+    $result=queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows) {
+        $row=$result->fetch_array(MYSQLI_ASSOC);
+        echo stripslashes($row['text']) . "<br style='clear:left'> <br>";
+
+    }else{
+        echo "<p> Nada para mostrar aqui, aún...</p> <br>";
+    }
+}
+function mysqlFatalError($mensaje)
+{
+    echo <<<_M
+    Lo sentimos, pero no fue posible completar la tarea requerida. el mensaje de error es:
+
+    <p> $mensaje <p>
+
+    Por favor haga click en el boton de atras en tu navegador e intenta de nuevo. si aun sigues teniendo problemas, por favor
+    <a href='mailto:admin@server.com'>email our administrator </a>. Thank you
+
+    _M;
+
+}
+?>
---
+++ proyecto-asamblea/header.php
@@ -0,0 +1,81 @@
+<?php
+
+session_start();
+
+echo <<<_INIT
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicativo Asambleas</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="js.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
+    <script src="https://kit.fontawesome.com/78e00f4d2a.js" crossorigin="anonymous"></script>
+_INIT;
+
+require_once "functions.php";
+
+$userstr="Bienvenido visitante";
+
+if (isset($_SESSION['user'])) {
+    $user=$_SESSION['user'];
+
+    $loggedin=TRUE;
+    $userstr=`Sesion iniciada como: $user`;
+
+}else{
+
+$loggedin=FALSE;
+
+}
+
+echo <<<_MAIN 
+<body>
+    <section id="page">
+    <header id="header">
+    <div id="logo" class="center">
+    <img src="rodri.gif" alt="gif">
+    Aplicativo de Asambleas
+    </div>
+    </header>
+    <p class="username">$userstr</p>
+    </section>
+    <section id="content">
+_MAIN;
+
+if ($loggedin) {
+    echo <<<_LOGGEDIN
+        <nav class="menu center">
+            <ul class="menu-list">
+                <li class="menu-list_item"><i class="fa-solid fa-house"></i><a href="members.php?view='$user'">Inicio</a></li>
+                <li class="menu-list_item"><i class="fa-solid fa-user-group"></i><a href="members.php">Miembros</a></li>
+                <li class="menu-list_item"><i class="fa-solid fa-sitemap"></i><a href="hierarchies.php">Jerarquías</a></li>
+                <li class="menu-list_item"><i class="fa-brands fa-meetup"></i><a href="meeting.php">Reuniones</a></li>
+                <li class="menu-list_item"><i class="fa-solid fa-user"></i><a href="profile.php">Perfiles</a></li>
+                <li class="menu-list_item"><i class="fa-solid fa-arrow-right-from-bracket"></i><a href="logout">Salir</a></li>
+            </ul>
+        </nav>
+
+    _LOGGEDIN;
+}else{
+    echo <<<_GUEST
+
+    <nav class="menu center">
+        <ul class="menu-list">
+        <li class="menu-list_item"><i class="fa-solid fa-house"></i><a href="home.php'">Inicio</a></li>
+        <li class="menu-list_item"><i class="fa-solid fa-user-plus"></i><a href="signup.php">Registrarse</a></li>
+        <li class="menu-list_item"><i class="fa-solid fa-arrow-right-to-arc"></i><a href="login.php">Iniciar Sesión</a></li>
+        </ul>
+    </nav>
+<p class='notice-not-logged'>Debes estar registrado e iniciar sesión para usar esta aplicación</p>
+
+
+    _GUEST;
+}
+
+?>
